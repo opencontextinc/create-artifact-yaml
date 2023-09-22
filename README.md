a4d84beb6088219b3ef96e6cfc2b78059a981ad3
Alice Chen <alchen99@users.noreply.github.com>
2023-09-22 10:54:27 -0700

    feat: Update GitHub Action (#2)
    - Update installed packages in container
    - Add LICENSE
    - Add CODEOWNERS
    - Add pull request template
    - Add/modify yaml templates
    - Handle GITHUB_ACTOR that is bot
    - Add directory input
    - Change output filename to be the tgz file that is create
    - Add directory output
    - Add CI workflow to check shell script
    
    ---------
    
    Co-authored-by: Alice Chen <alice@opencontext.com>

--- README.md
+++ README.md
@@ -122,3 +122,4 @@ In order for OpenContext to process these files you will need to do the followin
   - Commit these files to a repository that is for OpenContext YAML. For example, see the [opencontext repository](https://github.com/scatter-ly/opencontext) or the [scatter.ly repository](https://github.com/scatter-ly/scatter.ly) in our demo GitHub organization [scatter-ly](https://github.com/scatter-ly).
   - Concatenate the contents of the files generated into an `oc-catalog.yaml` file and commit it to the root of your current repository. For example, see the [retail-app repository](https://github.com/scatter-ly/retail-app) in [scatter-ly](https://github.com/scatter-ly), our demo GitHub organization.
   - Upload these files to the catalog files location for your tenant. See our [docs](https://docs.opencontext.com/docs/getting-started/client-portal#catalog-files) for more information.
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,37 @@
 #!/bin/sh
+# shellcheck disable=SC3057
 
 export artifactType="${INPUT_TYPE}"
 export url="${INPUT_URL}"
 export fullname="${url##*/}"
-export shortname="${fullname:0:63}"
+export ghaPath="${GITHUB_WORKSPACE}/${INPUT_DIRECTORY:-oc-artifact-yaml}"
+export tgzFile="oc-artifact-yaml.tgz"
 export tplName=""
 export filename=""
-
-env
-jq . $GITHUB_EVENT_PATH
+export shortname=""
 
 case "${artifactType}" in
-  sbom|SBOM|package)
-                    tplName="/templates/cc.yaml"
-                    filename="${GITHUB_WORKSPACE}/cc-${fullname}.yaml"
+  sbom)
+                    shortname="${fullname:0:63}"
+                    tplName="/templates/auxcomponent.yaml"
+                    filename="${ghaPath}/ac-${fullname}.yaml"
+                ;;
+  package)
+                    tShortName="${fullname%%#*}"
+                    shortname="${tShortName:0:63}"
+                    tplName="/templates/codecomponent.yaml"
+                    filename="${ghaPath}/cc-${shortname}.yaml"
                 ;;
   container|image)  
-                    tplName="/templates/pc.yaml"
-                    filename="${GITHUB_WORKSPACE}/pc-${fullname}.yaml"
+                    if [ "$artifactType" = "container" ]; then
+                      tShortName="${fullname%%:*}"
+                      shortname="${tShortName:0:63}"
+                      filename="${ghaPath}/pc-${shortname}.yam"
+                    else
+                      shortname="${fullname:0:63}"
+                      filename="${ghaPath}/pc-${fullname}.yaml"
+                    fi
+                    tplName="/templates/platformcomponent.yaml"
                 ;;
   *) echo "ERROR: Unknown artifact type!"
      exit 1
@@ -25,9 +39,33 @@ esac
 
 export ghaRunUrl="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
 export ghaRunTitle="GitHub Action Run ${GITHUB_RUN_ID}"
+# remove [bot] from the end of GITHUB_ACTOR
+export parsedGHActor="${GITHUB_ACTOR%%[bot*}"
+export ghBotAvatar="https://github.com/identicons/app/app/${parsedGHActor}"
 
-yq '.metadata.namespace = env(GITHUB_REPOSITORY_OWNER) | .metadata.name = env(shortname) | .metadata.title = env(fullname) | .metadata.links.[0].url = env(url) | .metadata.links.[0].title = env(fullname) |
-    .metadata.links.[1].url = env(ghaRunUrl) | .metadata.links.[1].title = env(ghaRunTitle) | .metadata.annotations."github.com/project-slug" = env(GITHUB_REPOSITORY) | .spec.type = env(artifactType) | .spec.uri = env(url) |
-    .spec.owner[0] = env(GITHUB_REPOSITORY_OWNER) + "/" + env(GITHUB_ACTOR) | .spec.dependsOn[0] = "codecomponent:" + env(GITHUB_REPOSITORY)' "${tplName}" > "${filename}"
+mkdir "$ghaPath"
 
-echo "filename=${filename}" >> "$GITHUB_OUTPUT"
+# create artifact YAML
+if [ "${artifactType}" = "container" ] || [ "${artifactType}" = "image" ]; then
+  yq '.metadata.namespace = env(GITHUB_REPOSITORY_OWNER) | .metadata.name = env(shortname) | .metadata.title = env(fullname) |
+      .metadata.links.[0].url = env(ghaRunUrl) | .metadata.links.[0].title = env(ghaRunTitle) | .metadata.annotations."github.com/project-slug" = env(GITHUB_REPOSITORY) | .spec.type = env(artifactType) | .spec.uri = env(url) |
+      .spec.owner[0] = env(GITHUB_REPOSITORY_OWNER) + "/" + env(parsedGHActor) | .spec.dependsOn[0] = "codecomponent:" + env(GITHUB_REPOSITORY)' "${tplName}" > "${filename}"
+else
+  yq '.metadata.namespace = env(GITHUB_REPOSITORY_OWNER) | .metadata.name = env(shortname) | .metadata.title = env(fullname) | .metadata.links.[0].url = env(url) | .metadata.links.[0].title = env(fullname) |
+      .metadata.links.[1].url = env(ghaRunUrl) | .metadata.links.[1].title = env(ghaRunTitle) | .metadata.annotations."github.com/project-slug" = env(GITHUB_REPOSITORY) | .spec.type = env(artifactType) | .spec.uri = env(url) |
+      .spec.owner[0] = env(GITHUB_REPOSITORY_OWNER) + "/" + env(parsedGHActor) | .spec.dependsOn[0] = "codecomponent:" + env(GITHUB_REPOSITORY)' "${tplName}" > "${filename}"
+fi
+
+# create YAML for bot
+if [ "$GITHUB_ACTOR" != "$parsedGHActor" ]; then
+  yq '.metadata.namespace = env(GITHUB_REPOSITORY_OWNER) | .metadata.name = env(parsedGHActor) | .metadata.annotations."github.com/project-slug" = env(GITHUB_REPOSITORY) | .spec.profile.picture = env(ghBotAvatar)' /templates/person.yaml > "${ghaPath}/${parsedGHActor}.yaml"
+fi
+
+# create tar-gzipped file of YAML generated
+cd "${ghaPath}" || exit
+tar cfz "${GITHUB_WORKSPACE}/${tgzFile}" .
+
+# set GHA output filename to tar-gzipped file generated
+echo "filename=${tgzFile}" >> "$GITHUB_OUTPUT"
+# set GHA output directory to the directory where the YAML files were generated
+echo "directory=${ghaPath##*/}" >> "$GITHUB_OUTPUT"
